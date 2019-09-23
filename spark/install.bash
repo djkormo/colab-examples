c6bb759d8123a7770169ad64fcc5dc1d744c8d07
Twoje imię i nazwsko <djkormo@gmail.com>
2019-09-23 23:37:54 +0200

    correcting install.bash

--- spark/install.bash
+++ spark/install.bash
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-apt-get install openjdk-8-jdk-headless -qq > /dev/null
-wget -q http://apache.crihan.fr/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
+cd /
+
+mkdir content
+
+cd content/ 
+
+apt-get install openjdk-8-jdk-headless
+wget http://apache.crihan.fr/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
 tar xf spark-2.4.4-bin-hadoop2.7.tgz
 pip install -q findspark
 pip install spark-nlp
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export SPARK_HOME=/content/spark-2.4.4-bin-hadoop2.7
