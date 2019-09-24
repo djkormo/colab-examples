24b8513af724954276103ceb813db0e1b585067b
djkormo <kpudlowski@plus.pl>
2019-09-24 14:42:33 +0200

    correcting run-tmux-jupyter.bash

--- ssh/run-tmux-jupyter.bash
+++ ssh/run-tmux-jupyter.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 tmux new-session -d 'jupyter notebook --ip 0.0.0.0 --NotebookApp.token='' --no-browser'
-tmux split-window -v 'ssh -R myjupyter-lab:80:localhost:8888 serveo.net'
+tmux split-window -v 'ssh -R myjupyter-lab:80:localhost:8888 serveo.net StrictHostKeyChecking=accept-new'
 tmux split-window -h
-tmux new-window 'nvtop'
+tmux new-window 'export LD_PRELOAD=/usr/lib64-nvidia/libnvidia-ml.so; nvtop'
 tmux -2 attach-session -d
