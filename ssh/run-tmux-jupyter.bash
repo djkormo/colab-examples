99ecc662e37fc0f4a3bc0685253da91e6a995806
djkormo <kpudlowski@plus.pl>
2019-09-26 10:11:36 +0200

    correcting  run-tmux-jupyter.bash

--- ssh/run-tmux-jupyter.bash
+++ ssh/run-tmux-jupyter.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# based on https://stackoverflow.com/questions/5609192/how-to-set-up-tmux-so-that-it-starts-up-with-specified-windows-opened
+
 export LD_PRELOAD=/usr/lib64-nvidia/libnvidia-ml.so
 tmux new-session -d 'jupyter notebook --ip 0.0.0.0 --NotebookApp.token='' --no-browser'
 tmux split-window -v 'ssh -R myjupyter-lab:80:localhost:8888 serveo.net StrictHostKeyChecking=no'
