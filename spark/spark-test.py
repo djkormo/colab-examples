e2f9051ec50038ee95bcfe9f3656368e46a014f2
Krzysztof Pudłowski <kpudlowski@plus.pl>
2019-10-15 08:32:36 +0200

    correcting spark-test.py

--- spark/spark-test.py
+++ spark/spark-test.py
@@ -1,26 +1,12 @@
 import os
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
 os.environ["SPARK_ME"] = "/content/spark-2.4.4-bin-hadoop2.7"
-
-import findspark
-findspark.init()
-from pyspark.sql import SparkSession
-
-import sparknlp 
-spark = sparknlp.start()
-from sparknlp.pretrained import PretrainedPipeline
-pipeline = PretrainedPipeline('recognize_entities_dl', 'en')
-result = pipeline.annotate('Harry Potter is a great movie')
-print(result['ner'])
-pipeline = PretrainedPipeline('analyze_sentiment', 'en')
-result = pipeline.annotate('Harry Potter is a great movie')
-print(result['sentiment'])
-
-
 # Create a local Spark Session
 
 import findspark
-findspark.init()
+findspark.init("spark-2.4.4-bin-hadoop2.7")
+from pyspark.sql import SparkSession
+
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.master("local[*]").getOrCreate()
 
@@ -28,3 +14,4 @@ spark = SparkSession.builder.master("local[*]").getOrCreate()
 
 df = spark.createDataFrame([{"Google": "Colab","Spark": "Scala"} ,{"Google": "Dataproc","Spark":"Python"}])
 df.show()
+
