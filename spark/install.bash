e4dd50e6a6517898c6e1e0595aaf6d0c2f83b77f
Krzysztof Pudłowski <kpudlowski@plus.pl>
2019-10-15 08:22:00 +0200

    correcring install.bash

--- spark/install.bash
+++ spark/install.bash
@@ -20,15 +20,5 @@ pip install spark-nlp
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export SPARK_HOME=/content/spark-2.4.4-bin-hadoop2.7
 
-
-# Create a local Spark Session
-
-import findspark
-findspark.init()
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.master("local[*]").getOrCreate()
-
-# Test Installation 
-
-df = spark.createDataFrame([{"Google": "Colab","Spark": "Scala"} ,{"Google": "Dataproc","Spark":"Python"}])
-df.show()
+# testing installation 
+python spark-test.py
--- spark/spark-test.py
+++ spark/spark-test.py
@@ -15,3 +15,16 @@ print(result['ner'])
 pipeline = PretrainedPipeline('analyze_sentiment', 'en')
 result = pipeline.annotate('Harry Potter is a great movie')
 print(result['sentiment'])
+
+
+# Create a local Spark Session
+
+import findspark
+findspark.init()
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local[*]").getOrCreate()
+
+# Test Installation 
+
+df = spark.createDataFrame([{"Google": "Colab","Spark": "Scala"} ,{"Google": "Dataproc","Spark":"Python"}])
+df.show()
