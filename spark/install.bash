688bd1d27f432d93865651582b47327540fb8d38
Krzysztof Pudłowski <kpudlowski@plus.pl>
2019-10-15 08:16:44 +0200

    correcting install.bash

--- spark/install.bash
+++ spark/install.bash
@@ -2,15 +2,33 @@
 
 cd /
 
-mkdir content
+#mkdir content
 
 cd content/ 
 
-apt-get install openjdk-8-jdk-headless
+apt-get install openjdk-8-jdk-headless -qq > /dev/null
+
 wget http://apache.crihan.fr/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
+
 tar xf spark-2.4.4-bin-hadoop2.7.tgz
+
 pip install -q findspark
+
 pip install spark-nlp
+
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export SPARK_HOME=/content/spark-2.4.4-bin-hadoop2.7
+
+
+# Create a local Spark Session
+
+import findspark
+findspark.init()
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local[*]").getOrCreate()
+
+# Test Installation 
+
+df = spark.createDataFrame([{"Google": "Colab","Spark": "Scala"} ,{"Google": "Dataproc","Spark":"Python"}])
+df.show()
