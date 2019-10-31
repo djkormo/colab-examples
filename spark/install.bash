cd21abe2fbfbf826f0e154e59c908f2cebdaa812
Twoje imię i nazwsko <djkormo@gmail.com>
2019-11-01 00:40:50 +0100

    Correcting SPARK_HOME

--- spark/install.bash
+++ spark/install.bash
@@ -7,7 +7,8 @@ mkdir -p content
 cd content/ 
 
 apt-get install openjdk-8-jdk-headless
-wget http://apache.crihan.fr/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
+
+wget http://apache.crihan.fr/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz -O spark-2.4.4-bin-hadoop2.7.tgz
 
 tar xf spark-2.4.4-bin-hadoop2.7.tgz
 
@@ -15,4 +16,4 @@ pip install -q findspark
 pip install spark-nlp
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export SPARK_HOME=export SPARK_HOME=/content/spark-2.4.4-bin-hadoop2.7
+export SPARK_HOME=/content/spark-2.4.4-bin-hadoop2.7
