fb756f5b59ff5a1b4b4e169e83265f2f42f83cda
Twoje imię i nazwsko <djkormo@gmail.com>
2019-10-31 23:19:00 +0100

    correctinng spark version

--- spark/install.bash
+++ spark/install.bash
@@ -8,8 +8,11 @@ cd content/
 
 apt-get install openjdk-8-jdk-headless
 wget http://apache.crihan.fr/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
-tar xf spark-2.4.4-bin-hadoop2.7.tgz
-mv spark-2.4.2-bin-hadoop2.7/ /opt/spark 
+
+tar xvf spark-2.4.4-bin-hadoop2.7.tgz
+
+mv spark-2.4.4-bin-hadoop2.7/ /opt/spark 
+
 pip install -q findspark
 pip install spark-nlp
 
