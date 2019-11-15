73949bd1c83f18e22aaf898833cbd68ec2847fcc
Krzysztof Pudłowski <kpudlowski@plus.pl>
2019-11-15 08:33:33 +0100

    adding nvtop install-additional.bash

--- spark/install-additional.bash
+++ spark/install-additional.bash
@@ -6,8 +6,20 @@ cd /
 
 cd content/ 
 
-apt-get install nmap net-tools lynx mc -y -qqq
+apt-get install nmap net-tools lynx mc htop -y -qqq
 
 apt-get install firefox -y -qqq
 
 
+apt-get install cmake libncurses5-dev libncursesw5-dev git -y -qqq
+
+git clone https://github.com/Syllo/nvtop.git
+mkdir -p nvtop/build && cd nvtop/build
+cmake ..
+
+# If it errors with "Could NOT find NVML (missing: NVML_INCLUDE_DIRS)"
+# try the following command instead, otherwise skip to the build with make.
+cmake .. -DNVML_RETRIEVE_HEADER_ONLINE=True
+
+make
+make install # You may need sufficient permission for that (root)
