df8ba9b8e4313ef094b9acc64ceafefd05ea27e7
Krzysztof Pudłowski <djkormo@gmail.com>
2021-05-30 16:24:19 +0200

    adding spark 2.4.8 version

--- spark/install.bash
+++ spark/install.bash
@@ -8,12 +8,13 @@ cd content/
 
 apt-get install openjdk-8-jdk-headless
 
-wget http://apache.crihan.fr/dist/spark/spark-2.4.6/spark-2.4.6-bin-hadoop2.7.tgz -O spark-2.4.6-bin-hadoop2.7.tgz
+# wget http://apache.crihan.fr/dist/spark/spark-2.4.6/spark-2.4.6-bin-hadoop2.7.tgz -O spark-2.4.6-bin-hadoop2.7.tgz 
+wget https://archive.apache.org/dist/spark/spark-2.4.8/spark-2.4.8-bin-hadoop2.7.tgz -O spark-2.4.8-bin-hadoop2.7.tgz
 
-tar xf spark-2.4.6-bin-hadoop2.7.tgz
+tar xf spark-2.4.8-bin-hadoop2.7.tgz
 
 pip install -q findspark
 pip install spark-nlp
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export SPARK_HOME=/content/spark-2.4.6-bin-hadoop2.7
+export SPARK_HOME=/content/spark-2.4.8-bin-hadoop2.7
