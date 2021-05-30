08273348d5c5778bda84532c83e65ef686ac0bf4
Krzysztof Pudłowski <djkormo@gmail.com>
2021-05-30 16:28:16 +0200

    adding spark 2.4.8 version

--- spark/spark-test.py
+++ spark/spark-test.py
@@ -1,10 +1,10 @@
 import os
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
-os.environ["SPARK_ME"] = "/content/spark-2.4.4-bin-hadoop2.7"
+os.environ["SPARK_ME"] = "/content/spark-2.4.8-bin-hadoop2.7"
 # Create a local Spark Session
 
 import findspark
-findspark.init("spark-2.4.4-bin-hadoop2.7")
+findspark.init("spark-2.4.8-bin-hadoop2.7")
 from pyspark.sql import SparkSession
 
 from pyspark.sql import SparkSession
